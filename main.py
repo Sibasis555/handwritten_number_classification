264a4068a7e46a3646317d2de53117bdfb7fbc52
Sibasis555 <sahoosibasis555@gmail.com>
2023-06-28 16:46:39 +0530

    data ingestion process

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from hwn_cnnClassifier import logger
+from hwn_cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from hwn_cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+# from hwn_cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+# from hwn_cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
-logger.info("Wlcome to the first log")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
